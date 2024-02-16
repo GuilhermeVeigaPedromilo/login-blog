67092d3e60f1cbb58ec15e64cd5569cd41298a4d
Guilherme Veiga Pedromilo <guipedromilo@gmail.com>
2024-02-15 22:42:01 -0300

    up db

--- mydb.sql
+++ mydb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 07/02/2024 às 15:24
--- Versão do servidor: 8.0.36-0ubuntu0.22.04.1
--- Versão do PHP: 8.1.2-1ubuntu2.14
+-- Tempo de geração: 15-Fev-2024 às 22:41
+-- Versão do servidor: 8.0.34-0ubuntu0.22.04.1
+-- versão do PHP: 8.1.2-1ubuntu2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,70 +24,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `posts`
+-- Estrutura da tabela `posts`
 --
 
 CREATE TABLE `posts` (
   `id` int NOT NULL,
   `titulo` text NOT NULL,
   `conteudo` text NOT NULL,
-  `autor` text NOT NULL,
-  `datapostagem` date NOT NULL
+  `nome` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `data` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Despejando dados para a tabela `posts`
---
-
-INSERT INTO `posts` (`id`, `titulo`, `conteudo`, `autor`, `datapostagem`) VALUES
-(3, 'SENAI', 'Dia 7 de fevereiro de 2024', 'admin', '2024-02-07'),
-(4, 'SENAI', '13:50', 'admin', '2024-02-07');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `typeuser` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabela de usuários';
 
 --
--- Despejando dados para a tabela `users`
+-- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'aluno', '23a6a3cf06cfd8b1a6cda468e5756a6a6a1d21e7'),
-(2, 'Guilherme Ped', '8cb2237d0679ca88db6464eac60da96345513964');
+INSERT INTO `users` (`id`, `username`, `password`, `typeuser`) VALUES
+(2, 'Guilherme Ped', '8cb2237d0679ca88db6464eac60da96345513964', 'admin');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `posts`
+-- Índices para tabela `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `users`
+-- Índices para tabela `users`
 --
 ALTER TABLE `users`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `users`
